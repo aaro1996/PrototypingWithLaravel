a73c1d8dc888dcbf8f1fbbdb621d6316ed8f5b2a
Aaron Handleman <aaro1996@gmail.com>
2017-05-11 01:11:34 -0400

    should ignore the start of the github header now

--- app/Http/Controllers/GitController.php
+++ app/Http/Controllers/GitController.php
@@ -10,7 +10,7 @@ class GitController extends Controller
 {
 	public function gitpull(Request $request) {
 		$gitkey = hash_hmac("sha1", file_get_contents("php://input"),"testkey");
-		$secret = getallheaders()['X-Hub-Signature'];
+		$secret = substr(getallheaders()['X-Hub-Signature'], 5);
 		$output = $secret;
 		if ($secret == null) {
 			$output = 'nokey';
@@ -18,9 +18,9 @@ class GitController extends Controller
 		if($request->has('ref') && $request->input('ref') === "refs/heads/master" && md5($secret) === md5($gitkey))
 		{
 			shell_exec("/usr/bin/git pull origin master");
-			$output = 'Success!!!!';
+			return 'Success!!!!';
 		}
-		return $output;
+		return ("Secret was: " + $secret + " Gitkey was: " + $gitkey;
 	}
 
 }
