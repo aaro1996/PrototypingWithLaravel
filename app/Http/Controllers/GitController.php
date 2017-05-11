7ee04b9328c8f06d1cab7c62de4aba9b5238e72f
Aaron Handleman <aaro1996@gmail.com>
2017-05-11 01:30:55 -0400

    I think I'm parsing the data correctly now

--- app/Http/Controllers/GitController.php
+++ app/Http/Controllers/GitController.php
@@ -9,21 +9,22 @@ use Illuminate\Http\Request;
 class GitController extends Controller
 {
 	public function gitpull(Request $request) {
-		$gitkey = hash_hmac("sha1", file_get_contents("php://input"),"testkey");
-		$headers = getallheaders();
-		$secret = substr($headers['X-Hub-Signature'], 5);
+		$input = file_get_contents("php://input");
+		$gitkey = hash_hmac("sha1", $input,"testkey");
+		$secret = substr(getallheaders()['X-Hub-Signature'], 5);
 		$output = $secret;
 		if ($secret == null) {
 			$output = 'nokey';
 		}
-		if(isset($_POST['ref']) && $_POST['ref'] === "refs/heads/master" && md5($secret) === md5($gitkey))
+		$json = json_decode($input);
+		if(isset($json['ref']) && $json['ref'] === "refs/heads/master" && md5($secret) === md5($gitkey))
 		{
 			shell_exec("/usr/bin/git pull origin master");
 			return 'Success!!!!';
 		}
 		return ("Secret was: " . $secret . " Gitkey was: " . $gitkey . "\n" . 
-			(isset($_POST['ref']) ? "Request had ref" : "Request didn't have ref") . "\n" . 
-			(isset($_POST['ref']) && $_POST['ref'] === "refs/heads/master" ? "Request was master" : "Request was not master") . "\n" .
+			(isset($json['ref']) ? "Request had ref" : "Request didn't have ref") . "\n" . 
+			(isset($json['ref']) && $json['ref'] === "refs/heads/master" ? "Request was master" : "Request was not master") . "\n" .
 			( md5($secret) === md5($gitkey) ? "Keys were equal" : "Keys weren't equal")
 			);
 	}
