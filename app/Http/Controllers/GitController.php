2b3afe52b4e673d229766ab3bd8a9fdfcf687e3b
Aaron Handleman <aaro1996@gmail.com>
2017-05-10 23:32:59 -0400

    now?

--- app/Http/Controllers/GitController.php
+++ app/Http/Controllers/GitController.php
@@ -10,11 +10,12 @@ class GitController extends Controller
 {
 	public function gitpull(Request $request) {
 		$gitkey = "testkey";
-
+		$output = "failed";
 		if($request->isMethod('post') && $request->has('ref') && $request->input('ref') === "refs/heads/master" && $request->input('hook')['secret'] === $gitkey)
 		{
-			echo shell_exec("/usr/bin/git pull origin master");
+			$output = shell_exec("/usr/bin/git pull origin master");
 		}
+		return $output;
 	}
 
 }
