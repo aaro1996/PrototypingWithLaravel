f70dbc283afe3e6f537066d87d348c6d9cd7e3f9
Aaron Handleman <aaro1996@gmail.com>
2017-05-11 00:46:38 -0400

    added better checking

--- app/Http/Controllers/GitController.php
+++ app/Http/Controllers/GitController.php
@@ -10,9 +10,11 @@ class GitController extends Controller
 {
 	public function gitpull(Request $request) {
 		$gitkey = "testkey";
-		$output = "no key";
 		$secret = $request->input('hook.secret');
 		$output = $secret;
+		if ($request->input('hook.secret') !== $gitkey) {
+			$output = 'nokey';
+		}
 		if($request->has('ref') && $request->input('ref') === "refs/heads/master" && $secret === $gitkey)
 		{
 			shell_exec("/usr/bin/git pull origin master");
