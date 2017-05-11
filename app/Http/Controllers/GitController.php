cd82d714950b72a75572df9021eb17e1ea234147
Aaron Handleman <aaro1996@gmail.com>
2017-05-11 00:02:23 -0400

    reads secret better maybe?

--- app/Http/Controllers/GitController.php
+++ app/Http/Controllers/GitController.php
@@ -11,8 +11,14 @@ class GitController extends Controller
 	public function gitpull(Request $request) {
 		$gitkey = "testkey";
 <<<<<<< HEAD
+<<<<<<< HEAD
 		$output = "failed";
 		if($request->isMethod('post') && $request->has('ref') && $request->input('ref') === "refs/heads/master" && $request->input('hook')['secret'] === $gitkey)
+=======
+		$secret = $request->input('hook.secret');
+		$output = $secret;
+		if($request->has('ref') && $request->input('ref') === "refs/heads/master" && $secret === $gitkey)
+>>>>>>> bc5a4ce... reads secret better maybe?
 =======
 		$secret = $request->input('hook.secret');
 		$output = $secret;
