b147ce8ce60504aa6ca655606dcfa60a8073b0bb
Aaron Handleman <aaro1996@gmail.com>
2017-05-11 01:24:01 -0400

    maybe request just doesn't work???

--- app/Http/Controllers/GitController.php
+++ app/Http/Controllers/GitController.php
@@ -10,19 +10,20 @@ class GitController extends Controller
 {
 	public function gitpull(Request $request) {
 		$gitkey = hash_hmac("sha1", file_get_contents("php://input"),"testkey");
-		$secret = substr(getallheaders()['X-Hub-Signature'], 5);
+		$headers = getallheaders();
+		$secret = substr($headers['X-Hub-Signature'], 5);
 		$output = $secret;
 		if ($secret == null) {
 			$output = 'nokey';
 		}
-		if($request->has('ref') && $request->input('ref') === "refs/heads/master" && md5($secret) === md5($gitkey))
+		if(isset($_POST['ref']) && $_POST['ref'] === "refs/heads/master" && md5($secret) === md5($gitkey))
 		{
 			shell_exec("/usr/bin/git pull origin master");
 			return 'Success!!!!';
 		}
 		return ("Secret was: " . $secret . " Gitkey was: " . $gitkey . "\n" . 
-			($request->has('ref') ? "Request had ref" : "Request didn't have ref") . "\n" . 
-			($request->input('ref') === "refs/heads/master" ? "Request was master" : "Request was not master") . "\n" .
+			(isset($_POST['ref'] ? "Request had ref" : "Request didn't have ref") . "\n" . 
+			($_POST['ref'] === "refs/heads/master" ? "Request was master" : "Request was not master") . "\n" .
 			( md5($secret) === md5($gitkey) ? "Keys were equal" : "Keys weren't equal")
 			);
 	}
