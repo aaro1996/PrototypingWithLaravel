2580a944d8a907db9776fa70d0f8f6d293bff864
Aaron Handleman <aaro1996@gmail.com>
2017-05-11 16:31:46 -0400

    better than before

---
+++ resources/assets/js/components/Squareboard.vue
@@ -0,0 +1,17 @@
+<table class="gameboard">
+	@for($i = 0; $i < $y_count; $i++)
+		<tr>
+			@for($j = 0; $j < $x_count; $j++)
+				<td>
+					@include('gameboard.framework.tiles.square', ['x' => $j, 'y' => $i])
+				</td>
+			@endfor
+		</tr>
+	@endfor
+</table>
+
+<div class="square_tile x_{{$x}} y_{{$y}}">
+	<div>
+		<p> x: {{$x}} y: {{$y}} </p>
+	</div>
+</div>
