095d8b6561bd7e5b0aa6196f4f89c1c0683e1355
Aaron Handleman <aaro1996@gmail.com>
2017-05-10 23:52:55 -0400

    fixed?

--- app/Http/Controllers/GitController.php
+++ app/Http/Controllers/GitController.php
@@ -10,8 +10,9 @@ class GitController extends Controller
 {
 	public function gitpull(Request $request) {
 		$gitkey = "testkey";
-		$output = $request->input('hook')['secret'];
-		if($request->isMethod('post') && $request->has('ref') && $request->input('ref') === "refs/heads/master" && $request->input('hook')['secret'] === $gitkey)
+		$secret = $request->all()['hook']['secret'];
+		$output = $secret;
+		if($request->isMethod('post') && $request->has('ref') && $request->input('ref') === "refs/heads/master" && $secret === $gitkey)
 		{
 			$output = shell_exec("/usr/bin/git pull origin master");
 		}
